bbcf772814498f45a78ef15136edb92ab77808b0
Keith Wasielewski <binarylunchbox@gmail.com>
2015-07-24 02:19:32 -0700

    change handlebar template for #each statement and cleaned up main.js

--- main.js
+++ main.js
@@ -4,23 +4,19 @@ var baseUrl = 'http://localhost:3000';
 
 $(function() {
 
-  var allUsers = $.get(baseUrl + '/users')
-  var userPermissions = $.get(baseUrl + '/permissions')
-  var source = $('#user-template').html();
-  var userTmpl = Handlebars.compile(source);
-  var permSource = $('#permissions-template').html();
-  var permTmpl = Handlebars.compile(permSource);
+  var allUsers = $.get(baseUrl + '/users');
+  var userPermissions = $.get(baseUrl + '/permissions');
 
-  var dbUsers = [];
-
+  // Displays users when clicked first time. Does not duplicate if clicked again.
   $('.get-users').on('click', function() {
     if ($('li').length == 0) {
       getUsers();
     } else {
       console.log('users are already there')
     }
-  })
+  });
 
+  // Displays permissions per user. Does not duplicate if same button clicked again.
   $('body').on('click', '.permissions', function() {
     var thisBtn = this;
     if ($(this).parents('li').find('ul').length == 0) {
@@ -28,26 +24,26 @@ $(function() {
     } else {
       console.log('permissions are already listed')
     }
-  })
+  });
 
+  var source = $('#user-template').html();
+  var userTmpl = Handlebars.compile(source);
+
+  // Grab users from database and render them to the template.
   function getUsers() {
     allUsers
       .done(function(users) {
-        users.forEach(function(user){
-          var html = {
-            id: user.id,
-            name: user.name
-          }
-          dbUsers.push(user);
-          $('.users').append(userTmpl(html));
-        })
-      })
+          $('.users').append(userTmpl(users));
+      });
   }
 
+  var permSource = $('#permissions-template').html();
+  var permTmpl = Handlebars.compile(permSource);
+
+  // Grabs permissions from the database and displays corrects set of user permissions.
   function getPermissions(obj) {
     var btnId = $(obj).parents('li').attr('id');
     $(obj).parents('li').append('<ul></ul>')
-    console.log(btnId)
     userPermissions
       .done(function(permissions) {
         permissions.forEach(function(permission) {
@@ -58,8 +54,8 @@ $(function() {
             }
             $(obj).parents('li').find('ul').append(permTmpl(html));
           }
-        })
-      })
+        });
+      });
   }
 
 });
